068970e7eaaf9ee872c97b1da4f6420dbda2c3c5
xianglutao <294301492@qq.com>
2022-04-14 10:54:48 +0800

    feat: Layout样式

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,15 +1,34 @@
 <!--
  * @Author: your name
  * @Date: 2022-04-13 15:09:29
- * @LastEditTime: 2022-04-13 15:28:32
+ * @LastEditTime: 2022-04-14 10:51:07
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \v3-ts-tourdeer\src\layout\index.vue
 -->
 <template>
-  <div><AppMain /></div>
+  <div class="app-wrapper">
+    <Sidebar class="sidebar-container" />
+    <div>
+      <AppMain />
+    </div>
+  </div>
 </template>
 <script lang="ts" setup>
-// import { AppMain } from './components';
 import AppMain from './components/AppMain';
+import Sidebar from './components/Sidebar/index';
 </script>
+<style lang="scss" scoped>
+@import '~@/styles/mixin.scss';
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+
+  &.mobile.openSidebar {
+    position: fixed;
+    top: 0;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-19 10:10:42
- * @LastEditTime: 2022-03-29 10:05:10
+ * @LastEditTime: 2022-04-14 10:39:06
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \v3-pro\src\main.ts
@@ -15,6 +15,7 @@ import router from './router';
 import store from './store';
 import * as ElIcons from '@element-plus/icons-vue';
 import './permission';
+import '@/styles/index.scss';
 
 const app = createApp(App);
 
